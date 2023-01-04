c3f22fea238632bb9f20c9f8c63920078fe4d209
Ryan Culp <105468921+giovannidonkey@users.noreply.github.com>
2023-01-04 09:09:58 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+const list = document.querySelector('#list');
+const inp = document.querySelector('#inp');
+const f = document.querySelector('#new-task');
+
+
+
+f.addEventListener('submit',function(e){
+    
+  e.preventDefault();
+
+  if(inp.value != ''){
+    
+    let rmvBtn = document.createElement('button');
+    rmvBtn.innerText = 'X'; 
+
+    let newLi = document.createElement('li');
+    newLi.innerText = inp.value;
+
+    newLi.appendChild(rmvBtn);
+    list.appendChild(newLi);
+  }
+    
+  f.reset();
+});
+
+list.addEventListener("click", function(e) {
+
+    const t = e.target.tagName;
+
+    if (t === "LI") {
+      e.target.style.textDecoration = "line-through";
+    } else if (t === "BUTTON") {
+      e.target.parentNode.remove();
+    }
+    console.log(t);
+});
+    
+   
+   
